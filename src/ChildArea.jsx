2c6fdf0e90d43886f8779724ec5fe2dcbf93927e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-30 20:03:54 +0900

    css適用

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,9 +1,15 @@
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
 export const ChildArea = (props) => {
   const { open } = props;
   return (
     <>
       {open ? (
-        <div>
+        <div style={style}>
           <p>子コンポーネント</p>
         </div>
       ) : null}
